e00d62bcafc76f1d4e4f51e2d26b1211ea2b91ea
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2021-03-03 14:53:11 -0300

    Update SendEmail.router.js

--- middleware/SendEmail.router.js
+++ middleware/SendEmail.router.js
@@ -27,12 +27,7 @@ router.post('/recebidos', (req, res) => {
   
   // Mandando email
   const remetente = Nodemailer.createTransport(smtpTransport({
-    service: 'smtp.gmail.com',
-    port:465,
-    secure: true, // true for 465, false for other ports
-    logger: true,
-    debug: true,
-    secureConnection: false,
+    service: 'Hotmail',
     auth: {
       user: process.env.REMETENTE_EMAIL,
       pass: process.env.REMETENTE_PASS
